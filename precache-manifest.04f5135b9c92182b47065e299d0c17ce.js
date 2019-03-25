80f9b8faedbf0e488f3505cb563e0785bf307104
John Williamson <johnjwilliamson@live.com>
2019-03-25 13:48:46 +1000

    Updates

---
+++ precache-manifest.04f5135b9c92182b47065e299d0c17ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7a8ab2230734f3287b18",
+    "url": "/static/js/main.3640ca74.chunk.js"
+  },
+  {
+    "revision": "e1cab316d0d8af0f0152",
+    "url": "/static/js/2.d53a7cc4.chunk.js"
+  },
+  {
+    "revision": "7a8ab2230734f3287b18",
+    "url": "/static/css/main.f6da5935.chunk.css"
+  },
+  {
+    "revision": "8cd9cc50af7110e51af2524fc9c37a36",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f0e4c0e1ae2aedb5e66d6bf881a4d4e.js"
+  "/precache-manifest.04f5135b9c92182b47065e299d0c17ce.js"
 );
 
 workbox.clientsClaim();
